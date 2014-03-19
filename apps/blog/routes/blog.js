1399c5d70a5a637074888078aed03af4657b4df4
thisita <thisita@outlook.com>
2014-03-19 02:03:42 -0400

    Current working copy
    This is the current state of development. All changes shall be tracked
    now on GitHub.

--- apps/blog/routes/blog.js
+++ apps/blog/routes/blog.js
@@ -1,11 +1,63 @@
 var version = require('./version').string;
+var fs = require('fs');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
   res.render('index', {
-    pageTitle: 'Thisita: The Blog',
+    pageTitle: 'ThisitaNet Blog',
     siteVersion: version
   });
 };
+
+exports.post = function(req, res){
+  getArticle(req.params.year,
+    req.params.month,
+    req.params.day,
+    req.params.id,
+    function(err,data) {
+      if(err) {
+        res.render('postnotfound', {
+          pageTitle: 'ThisitaNet Blog',
+          siteVersion: version
+        });
+      } else {
+        res.render('post', {
+          pageTitle: 'ThisitaNet Blog: ' + data.title,
+          siteVersion: version,
+          post: data
+        });
+      }
+    }
+  );
+  res.render('post', {
+    pageTitle: 'ThisitaNet Blog: ',
+    siteVersion: version
+  });
+};
+
+// helper functions
+
+function getArticle(y,m,d,i,cb) {
+  var year = parseInt(y),
+    month = parseInt(m),
+    day = parseInt(d),
+    id = parseInt(i);
+  if(isNaN(year) || isNaN(month) || isNaN(day) || isNaN(id)) {
+    // somebody is pulling our leg
+    cb(true, null);
+  }
+  
+  var file = _dirname + '/public/json/';
+  file += year + '/' + month + '/' + day + '/' + id + '.json';
+  
+  fs.readFile(file, 'utf8', function(err,data) {
+    if(err) {
+      cb(true, null);
+    }
+    
+    var json = JSON.parse(data);
+    cb(false, json);
+  });
+}
--- apps/blog/routes/index.js
+++ apps/blog/routes/index.js
@@ -4,4 +4,6 @@ module.exports = function(app) {
   // Set up the routes
   app.get('/', function(req,res) { res.redirect('/index.html'); });
   app.get('/index.html', routes.index);
+  app.get('/posts', function(req,res) { res.redirect('/index.html'); });
+  app.get('/posts/:year/:month/:day/:id', routes.post);
 };
